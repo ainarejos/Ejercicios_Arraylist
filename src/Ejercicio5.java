6068521f4f98d487ec1cc6a0ad90c489a4e4795a
ainarejos <ainarejos@nigul.cide.es>
2021-01-13 20:39:48 +0100

    gos cat.

---
+++ src/Ejercicio5.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        HashMap<String, String> EllenPage = new HashMap<>();
+        EllenPage.put("Hola", "Hello");
+        EllenPage.put("Adeu", "Bye");
+        EllenPage.put("Cotxe", "Car");
+        EllenPage.put("Gos", "Dog");
+        EllenPage.put("Moix", "cat");
+        System.out.println(EllenPage);
+    }
+
+}
